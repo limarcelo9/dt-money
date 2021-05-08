5b499d862b8fe006199a7dcf891e40370f095ceb
unknown <limarcelo90@gmail.com>
2021-05-08 19:52:13 -0400

    Componentizando RadioBox e estilizando modal

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+
+  background: linear-gradient(217deg, var(--blue-light),  var(--blue) 70.71%);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 2rem 1rem 12rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    
+    font-size: 1rem;
+    color: #fff;
+
+    background: var(--blue);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+    transition: filter 0.2s;
+    
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+
+  
+`;
