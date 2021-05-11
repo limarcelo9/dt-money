9cc9aaf778a3d6b684d4d770a07d581ff301a401
unknown <limarcelo90@gmail.com>
2021-05-10 22:11:22 -0400

    Criando rota post de transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 import { Container, TrnsactionTypeContainer, RadioBox} from './styles'
 
 interface NewTransactionModalProps {
@@ -20,12 +21,14 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log(
+    const data = {
       title,
       category,
       value,
       type
-    )
+    }
+
+    api.post('/transactions', data)
   }
 
   return (
